33cdc61b794c66934ccd126e7686630b6bcb544a
Leona Yang <leona.y.young@gmail.com>
2021-12-10 16:24:06 +0800

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import React, { ReactElement } from 'react'
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import 'normalize.css/normalize.css'
 import '../styles/index.scss'
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-TEY4E3QCEY"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TEY4E3QCEY', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <script async src="/lib/autotrack.js"></script>
+      <Component {...pageProps} />
+    </>
+  )
 }
