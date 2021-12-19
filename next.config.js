de46f7505ecb2c844f5e9ef6467510947a5e2384
Leona Yang <leona.y.young@gmail.com>
2021-12-19 14:16:04 +0800

    add securityHeaders

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+ const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+]
+ const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
